1dd7275ebc5260d4225b8513c516d6d9a6f719eb
Hilaire Bekono <mbhd90@gmail.com>
2025-05-13 16:34:44 +0000

    fix pvc issues

--- deploy.sh
+++ deploy.sh
@@ -22,11 +22,11 @@ kubectl apply -f pvcs/sites-pv.yaml
 kubectl apply -f pvcs/sites-pvc.yaml
 
 # Deployments
-kubectl apply -f deployments/backend-deployment.yaml
-kubectl apply -f deployments/configurator-deployment.yaml
+#kubectl apply -f deployments/configurator-deployment.yaml
 # kubectl apply -f deployments/create-site-job.yaml
 kubectl apply -f deployments/db-deployment.yaml
-kubectl apply -f deployments/frontend-deployment.yaml
+kubectl apply -f services/db-service.yaml
+
 kubectl apply -f deployments/queue-default-deployment.yaml
 kubectl apply -f deployments/queue-long-deployment.yaml
 kubectl apply -f deployments/queue-short-deployment.yaml
@@ -36,8 +36,10 @@ kubectl apply -f deployments/redis-socketio-deployment.yaml
 kubectl apply -f deployments/scheduler-deployment.yaml
 kubectl apply -f deployments/websocket-deployment.yaml
 
+kubectl apply -f deployments/backend-deployment.yaml
+kubectl apply -f deployments/frontend-deployment.yaml
+
 # Services
-kubectl apply -f services/db-service.yaml
 kubectl apply -f services/redis-cache-service.yaml
 kubectl apply -f services/redis-queue-service.yaml
 kubectl apply -f services/redis-socketio-service.yaml
